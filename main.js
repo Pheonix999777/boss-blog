d4b644a64360548d0f5dc2a837067ce80a4d3324
Pheonix999777 <Pheonix4777@gmail.com>
2023-11-17 11:44:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,429 @@
+const data = {
+  posts: [
+    {
+      userId: 1,
+      id: 1,
+      title: "Post 1",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=1",
+    },
+    {
+      userId: 2,
+      id: 2,
+      title: "Post 2",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=2",
+    },
+    {
+      userId: 3,
+      id: 3,
+      title: "Post 3",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=3",
+    },
+    {
+      userId: 4,
+      id: 4,
+      title: "Post 4",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=4",
+    },
+    {
+      userId: 5,
+      id: 5,
+      title: "Post 5",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=5",
+    },
+    {
+      userId: 6,
+      id: 6,
+      title: "Post 6",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=6",
+    },
+    {
+      userId: 7,
+      id: 7,
+      title: "Post 7",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=7",
+    },
+    {
+      userId: 8,
+      id: 8,
+      title: "Post 8",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=8",
+    },
+    {
+      userId: 9,
+      id: 9,
+      title: "Post 9",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=9",
+    },
+    {
+      userId: 10,
+      id: 10,
+      title: "Post 10",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=10",
+    },
+    {
+      userId: 11,
+      id: 11,
+      title: "Post 11",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=11",
+    },
+    {
+      userId: 12,
+      id: 12,
+      title: "Post 12",
+      body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+      created_at: "2023-09-09T11:36:26.861Z",
+      views: 326,
+      image: "https://picsum.photos/360/300/?random=12",
+    },
+    {
+      userId: 1694421712678,
+      title: "Lorem Ipsum Dolor SIt emet",
+      body: "lasdf;aoksdf;alskjdfa;skdfja;slkdfja;sldkfjas;ldkfja;sldkfjas;dlkfj",
+      image: "/static/media/article.6e77bd5bce0396b4a36f.png",
+      created_at: "2023-09-11T08:41:52.678Z",
+      views: 354,
+      id: 13,
+    },
+    {
+      userId: 1694421863575,
+      title: "asdfasdf",
+      body: "afsddfsa",
+      image: "/static/media/article.6e77bd5bce0396b4a36f.png",
+      created_at: "2023-09-11T08:44:23.575Z",
+      views: 354,
+      id: 14,
+    },
+  ],
+  users: [
+    {
+      id: 1,
+      username: "user1",
+      password: "123",
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      phone: "1-770-736-8031 x56442",
+      image: "https://picsum.photos/360/300/?random=1",
+    },
+    {
+      id: 2,
+      username: "user2",
+      password: "123",
+      name: "Ervin Howell",
+      email: "Shanna@melissa.tv",
+      phone: "010-692-6593 x09125",
+      image: "https://picsum.photos/360/300/?random=2",
+    },
+    {
+      id: 3,
+      username: "user3",
+      password: "123",
+      name: "Clementine Bauch",
+      email: "Nathan@yesenia.net",
+      phone: "1-463-123-4447",
+      image: "https://picsum.photos/360/300/?random=3",
+    },
+    {
+      id: 4,
+      username: "user4",
+      password: "123",
+      name: "Patricia Lebsack",
+      email: "Julianne.OConner@kory.org",
+      phone: "493-170-9623 x156",
+      image: "https://picsum.photos/360/300/?random=4",
+    },
+    {
+      id: 5,
+      username: "user5",
+      password: "123",
+      name: "Chelsey Dietrich",
+      email: "Lucio_Hettinger@annie.ca",
+      phone: "(254)954-1289",
+      image: "https://picsum.photos/360/300/?random=5",
+    },
+    {
+      id: 6,
+      username: "user6",
+      password: "123",
+      name: "Mrs. Dennis Schulist",
+      email: "Karley_Dach@jasper.info",
+      phone: "1-477-935-8478 x6430",
+      image: "https://picsum.photos/360/300/?random=6",
+    },
+    {
+      id: 7,
+      username: "user7",
+      password: "123",
+      name: "Kurtis Weissnat",
+      email: "Telly.Hoeger@billy.biz",
+      phone: "210.067.6132",
+      image: "https://picsum.photos/360/300/?random=7",
+    },
+    {
+      id: 8,
+      username: "user8",
+      password: "123",
+      name: "Nicholas Runolfsdottir V",
+      email: "Sherwood@rosamond.me",
+      phone: "586.493.6943 x140",
+      image: "https://picsum.photos/360/300/?random=8",
+    },
+    {
+      id: 9,
+      username: "user9",
+      password: "123",
+      name: "Glenna Reichert",
+      email: "Chaim_McDermott@dana.io",
+      phone: "(775)976-6794 x41206",
+      image: "https://picsum.photos/360/300/?random=9",
+    },
+    {
+      id: 10,
+      username: "user10",
+      password: "123",
+      name: "Clementina DuBuque",
+      email: "Rey.Padberg@karina.biz",
+      phone: "024-648-3804",
+      image: "https://picsum.photos/360/300/?random=10",
+    },
+    {
+      id: 11,
+      username: "user11",
+      password: "123",
+      name: "Stefano Richi",
+      email: "steffi@karina.biz",
+      phone: "024-648-3804",
+      image: "https://picsum.photos/360/300/?random=11",
+    },
+    {
+      id: 12,
+      username: "user12",
+      password: "123",
+      name: "Fernadona DuBuque",
+      email: "ferrri@karina.biz",
+      phone: "024-648-3804",
+      image: "https://picsum.photos/360/300/?random=12",
+    },
+    {
+      id: 1694252587266,
+      username: "user1694252587266",
+      password: "123",
+      name: "Mrs. Dennis Schulist",
+      email: "Karley_Dach@jasper.info",
+      phone: "1-477-935-8478 x6430",
+      image: "https://picsum.photos/360/300/?random=6",
+    },
+    {
+      id: 1694252664022,
+      username: "user1694252664022",
+      password: "123",
+      name: "Mrs. Dennis Schulist",
+      email: "Karley_Dach@jasper.info",
+      phone: "1-477-935-8478 x6430",
+      image: "https://picsum.photos/360/300/?random=6",
+    },
+    {
+      id: 1694252668581,
+      username: "user1694252668581",
+      password: "123",
+      name: "Mrs. Dennis Schulist",
+      email: "Karley_Dach@jasper.info",
+      phone: "1-477-935-8478 x6430",
+      image: "https://picsum.photos/360/300/?random=6",
+    },
+    {
+      username: "user123123",
+      password: "123",
+    },
+  ],
+  avatars: [
+    {
+      id: 1,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-zena.jpg",
+    },
+    {
+      id: 2,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-elijah.jpg",
+    },
+    {
+      id: 3,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-george.jpg",
+    },
+    {
+      id: 4,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-james.jpg",
+    },
+    {
+      id: 5,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-javier.jpg",
+    },
+    {
+      id: 6,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-roxanne.jpg",
+    },
+    {
+      id: 7,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-ryan.jpg",
+    },
+  ],
+  history: [
+    {
+      id: 1,
+      isViewed: false,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-zena.jpg",
+    },
+    {
+      id: 2,
+      isViewed: true,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-elijah.jpg",
+    },
+    {
+      id: 3,
+      isViewed: false,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-george.jpg",
+    },
+    {
+      id: 4,
+      isViewed: false,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-james.jpg",
+    },
+    {
+      id: 5,
+      isViewed: true,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-javier.jpg",
+    },
+    {
+      id: 6,
+      isViewed: false,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-roxanne.jpg",
+    },
+    {
+      id: 7,
+      isViewed: false,
+      url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-ryan.jpg",
+    },
+  ],
+};
+
+let head = document.querySelector(".head__ul");
+
+head.setAttribute("class", "head__ul");
+
+for (let i = 0; i < 6; i++) {
+  let headMain = document.createElement("li");
+  headMain.setAttribute("class", "head__main");
+
+  let img = document.createElement("img");
+  img.setAttribute("src", data.posts[i].image);
+
+  let headWrapper = document.createElement("div");
+  headWrapper.setAttribute("class", "head__wrapper");
+
+  let headWords = document.createElement("div");
+  headWords.setAttribute("class", "head__words");
+
+  let strong = document.createElement("strong");
+  strong.setAttribute("class", "head__words-text");
+  strong.textContent = data.posts[i].userId;
+
+  let spn = document.createElement("span");
+  spn.setAttribute("class", "head__words-wrap");
+  spn.textContent = data.posts[i].id;
+
+  let headTitle = document.createElement("h2");
+  headTitle.setAttribute("class", "head__title");
+  headTitle.textContent = data.posts[i].title;
+  let headtext = document.createElement("p");
+  headtext.setAttribute("class", "head__wrapper-text");
+  headtext.textContent = data.posts[i].body;
+
+  let headWrapperDiv = document.createElement("div");
+  headWrapperDiv.setAttribute("class", "head__wrapper-div");
+
+  let svg = document.createElement("img");
+  svg.setAttribute("src", "img/photo.png");
+
+  let headwrapperwrapped = document.createElement("p");
+  headwrapperwrapped.setAttribute("class", "head__wrapper-wrapped");
+  headwrapperwrapped.textContent = data.users[i].name;
+  let headWrapperNames = document.createElement("div");
+  headWrapperNames.setAttribute("class", "head__wrapper-names");
+
+  let headWrapperWhite = document.createElement("div");
+  headWrapperWhite.setAttribute("class", "head__wrapper-white");
+
+  let pht = document.createElement("img");
+  pht.setAttribute("src", "img/Frame.svg");
+
+  let headwrapperWrapped2 = document.createElement("p");
+  headwrapperWrapped2.setAttribute("class", "head__wrapper-wrapped2");
+  headwrapperWrapped2.textContent = data.posts[i].views;
+
+  let headWhite = document.createElement("div");
+  headWhite.setAttribute("class", "head__wrapper-white");
+
+  let headWhite2 = document.createElement("div");
+  headWhite2.setAttribute("class", "head__wrapper-white");
+
+  let pht2 = document.createElement("img");
+  pht2.setAttribute("src", "img/svg3.svg");
+
+  let headwrapped3 = document.createElement("p");
+  headwrapped3.setAttribute("class", "head__wrapper-wrapped3");
+  headwrapped3.textContent = data.posts[i].userId;
+
+  let White = document.createElement("img");
+  White.setAttribute("src", "img/svg2.svg");
+
+  headMain.appendChild(img);
+  headWrapper.appendChild(headWords);
+  headWords.appendChild(spn);
+  headWords.appendChild(strong);
+  headWrapper.appendChild(headTitle);
+  headWrapper.appendChild(headtext);
+  headMain.appendChild(headWrapper);
+  headWrapper.appendChild(headWrapperDiv);
+  headWrapperDiv.appendChild(svg);
+  headWrapperDiv.appendChild(headwrapperwrapped);
+  headWrapperDiv.appendChild(headWrapperNames);
+  headWrapperNames.appendChild(headWrapperWhite);
+  headWrapperWhite.appendChild(pht);
+  headWrapperWhite.appendChild(headwrapperWrapped2);
+  headWrapperWhite.appendChild(pht2);
+  headWrapperWhite.appendChild(headwrapped3);
+  headWrapperWhite.appendChild(White);
+
+  head.appendChild(headMain);
+}
